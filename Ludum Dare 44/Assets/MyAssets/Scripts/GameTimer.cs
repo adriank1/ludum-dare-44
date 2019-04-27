85e60755249701aa815c9f2d645da83764952b5f
Adrian Kovatana <adrian.kovatana@gmail.com>
2019-04-27 07:59:01 -0500

    Add game timer

---
+++ Ludum Dare 44/Assets/MyAssets/Scripts/GameTimer.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace AdrianKovatana
+{
+    public class GameTimer : MonoBehaviour
+    {
+#pragma warning disable
+        [SerializeField]
+        private TextMeshProUGUI _textHHMMSS;
+        [SerializeField]
+        private TextMeshProUGUI _textMilliseconds;
+#pragma warning restore
+
+        private float _currentTime;
+
+        private void Awake()
+        {
+            _currentTime = 0f;
+        }
+
+        private void Update()
+        {
+            _currentTime += Time.unscaledDeltaTime;
+            UpdateUI();
+        }
+
+        public void UpdateUI()
+        {
+            _textHHMMSS.text = GetTime();
+            _textMilliseconds.text = GetMilliseconds();
+        }
+
+        public string GetTime()
+        {
+            string sHours, sMinutes, sSeconds;
+
+            sHours = Mathf.Floor(_currentTime / 3600).ToString("00");
+            sMinutes = Mathf.Floor(_currentTime / 60).ToString("00");
+            sSeconds = (_currentTime % 60).ToString("00");
+
+            return sHours + ":" + sMinutes + ":" + sSeconds;
+        }
+
+        public string GetMilliseconds()
+        {
+            return ":" + ((_currentTime * 1000) % 1000).ToString("000");
+        }
+    }
+}
