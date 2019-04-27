33bcc9b010bee12c2a409a7bc9faffa8ee7c5307
Adrian Kovatana <adrian.kovatana@gmail.com>
2019-04-27 10:52:20 -0500

    Add game time to game over screen

---
+++ Ludum Dare 44/Assets/MyAssets/Scripts/GameOverUI.cs
@@ -0,0 +1,56 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace AdrianKovatana
+{
+    public class GameOverUI : MonoBehaviour
+    {
+#pragma warning disable
+        [SerializeField]
+        private FloatVariable _currentTime;
+
+        [Header("UI Text")]
+        [SerializeField]
+        private TextMeshProUGUI _textHours;
+        [SerializeField]
+        private TextMeshProUGUI _textMinutes;
+        [SerializeField]
+        private TextMeshProUGUI _textSeconds;
+        [SerializeField]
+        private TextMeshProUGUI _textMilliseconds;
+#pragma warning restore
+
+        private void OnEnable()
+        {
+            UpdateUI();
+        }
+
+        public void UpdateUI()
+        {
+            _textHours.text = GetHours();
+            _textMinutes.text = ":" + GetMinutes();
+            _textSeconds.text = ":" + GetSeconds();
+            _textMilliseconds.text = ":" + GetMilliseconds();
+        }
+
+        public string GetHours()
+        {
+            return ((int)(_currentTime.value / 3600f) % 24).ToString("D2");
+        }
+
+        public string GetMinutes()
+        {
+            return ((int)(_currentTime.value / 60f) % 60).ToString("D2");
+        }
+
+        public string GetSeconds()
+        {
+            return ((int)(_currentTime.value % 60f)).ToString("D2");
+        }
+
+        public string GetMilliseconds()
+        {
+            return ((int)(_currentTime.value * 1000f) % 1000).ToString("D3");
+        }
+    }
+}
